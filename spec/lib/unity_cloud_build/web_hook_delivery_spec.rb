f220d5a4590317b191df0ce3a8d1ed8df7e7892f
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2017-10-28 19:51:56 +1100

    Parse body string to a hash and make it accessible

--- spec/lib/unity_cloud_build/web_hook_delivery_spec.rb
+++ spec/lib/unity_cloud_build/web_hook_delivery_spec.rb
@@ -26,4 +26,35 @@ RSpec.describe UnityCloudBuild::WebHookDelivery do
       end
     end
   end
+
+  describe '#json_body' do
+    subject { described_class.new(body, nil, nil, nil, nil).json_body }
+
+    context 'with a JSON parsable body' do
+      let(:body) {
+        '{"projectName":"MyProject","buildTargetName":"My Target Name","projectGuid":"aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee","orgForeignKey":"11111111111111","buildNumber":10,"buildStatus":"success","lastBuiltRevision":"30d852ea80c5c8e32cb36918e84f654e884e4b12"}'
+      }
+
+      it 'returns a valid Hash' do
+        is_expected.to match({
+          'projectName' => 'MyProject',
+          'buildTargetName' => 'My Target Name',
+          'projectGuid' => 'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee',
+          'orgForeignKey' => '11111111111111',
+          'buildNumber' => 10,
+          'buildStatus' => 'success',
+          'lastBuiltRevision' => '30d852ea80c5c8e32cb36918e84f654e884e4b12'
+        })
+      end
+    end
+
+    context 'with a JSON un-parsable body' do
+      let(:body) { 'invalid' }
+
+      it 'returns an empty Hash' do
+        is_expected.to be_a Hash
+        is_expected.to be_empty
+      end
+    end
+  end
 end
